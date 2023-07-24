838e3566a4c36b07e4ba97b61e58ef138de0d353
JohnnyMCR <jmcastillo991@gmail.com>
2023-07-23 22:31:41 -0400

    edited sql

--- db/seed.sql
+++ db/seed.sql
@@ -2,8 +2,8 @@
 
 
 INSERT INTO petitioners (id, name, borough, address, bin_mpg, bin_p) VALUES
-(01, 'Sam Smith', 'Brooklyn', '123 Main St', '1', '2'),
-(02, 'Theo Chandler', 'Queens', '456 Elm St', '4', '0'),
-(03, 'Veronica Abbrams', 'Bronx', '789 Oak St', '2', '1'),
-(04, 'Kathy Santiago', 'Manhattan', '567 Pine St', '2', '4'),
-(05, 'Timothy Williams', 'Staten Island', '234 Maple St', '2', '1');
+('Sam Smith', 'Brooklyn', '123 Main St', '1', '2'),
+('Theo Chandler', 'Queens', '456 Elm St', '4', '0'),
+('Veronica Abbrams', 'Bronx', '789 Oak St', '2', '1'),
+('Kathy Santiago', 'Manhattan', '567 Pine St', '2', '4'),
+('Timothy Williams', 'Staten Island', '234 Maple St', '2', '1');
